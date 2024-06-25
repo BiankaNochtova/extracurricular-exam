b373e272d0fab414dc4bc71768f0a89b26a541bb
BiankaNochtova <eaa23bino@students.eaaa.dk>
2024-06-25 11:13:20 +0200

    push
    blabla

--- app.js
+++ app.js
@@ -3,6 +3,36 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const projects = await getProjects(); // Call getProjects function to fetch project data
+  displayProjectsGrid(projects); // Call displayProjectsGrid function to display projects
+}
+
+async function getProjects() {
+  const response = await fetch("https://reexam2ndsemester.biankanochtova.dk/wp-json/wp/v2/posts?acf_format=standard"); // Fetch project data from API
+  const data = await response.json(); // Convert response to JSON format
+  return data; // Return the project data
+}
+function displayProjectsGrid(projects) {
+  const projectsGrid = document.querySelector("#projects-list"); // Select the container for projects
+  for (const project of projects) { // Loop through each project
+    projectsGrid.insertAdjacentHTML(
+      "beforeend", // Insert HTML at the end of the container
+      /*html*/ `
+      <article class="grid-item">
+        <img src="${project.acf.project_image}" alt="${project.title.rendered}" /> <!-- Display project image -->
+        <div class="grid-item-content">
+          <h2>${project.title.rendered}</h2> <!-- Display project title -->
+          <p>${project.acf.project_description}</p> <!-- Display project description -->
+        </div>
+        <div class="project-info">
+          <p>${project.acf.project_client}</p> <!-- Display project client -->
+          <a href="${project.acf.project_link}">Link to solution</a> <!-- Display link to project solution -->
+        </div>
+      </article>
+  `
+    //In this part the link and client are split from the image, description and title
+    );
+  }
 }
